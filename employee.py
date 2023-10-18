1bf8f6302b93b9926544f29cc370dc71686d0b76
Ahmet Kucuk <ahm3ttarik@gmail.com>
2023-10-18 11:51:33 +0100

    Pull if statement into parent class

--- employee.py
+++ employee.py
@@ -1,67 +1,75 @@
 class Employee:
-    def __init__(self, name, commission = None):
+    def __init__(self, name, commission=None):
         self.name = name
         self.commission = commission
 
     def get_pay(self):
         pass
 
+
     def __str__(self):
-        pass
+        return str(self.commission) if self.commission else ""
+
+
+    def get_commission(self):
+        return self.commission.calc_commission() if self.commission else 0
+
 
 class SalaryContract(Employee):
-    def __init__(self, name, salary, commission = None):
+    def __init__(self, name, salary, commission=None):
         super().__init__(name, commission)
         self.salary = salary
 
     def get_pay(self):
-        return self.salary + self.commission.get_commission() if self.commission else self.salary
+        return self.salary + self.get_commission()
 
     def __str__(self):
-        commission_info = self.commission.get_string() if self.commission else ""
-        return f"{self.name} works on a monthly salary of {self.salary}{commission_info}. Their total pay is {self.get_pay()}."
+        return f"{self.name} works on a monthly salary of {self.salary}{super().__str__()}. Their total pay is {self.get_pay()}."
+
 
 class HourlyContract(Employee):
-    def __init__(self, name, hours, hourly, commission = None):
+    def __init__(self, name, hours, hourly, commission=None):
         super().__init__(name, commission)
         self.hours = hours
         self.hourly = hourly
 
     def get_pay(self):
-        return self.hours * self.hourly + self.commission.get_commission() if self.commission else self.hours * self.hourly
+        return self.hours * self.hourly + self.get_commission()
 
     def __str__(self):
-        commission_info = self.commission.get_string() if self.commission else ""
-        return f"{self.name} works on a contract of {self.hours} hours at {self.hourly}/hour{commission_info}. Their total pay is {self.get_pay()}."
+        return f"{self.name} works on a contract of {self.hours} hours at {self.hourly}/hour{super().__str__()}. Their total pay is {self.get_pay()}."
 
 
 class Commission:
-    def get_commission(self):
+    def calc_commission(self):
         pass
 
-    def get_string(self):
+    def __str__(self):
         pass
+
 
 class FixedBonus(Commission):
     def __init__(self, fixed_bonus):
         self.fixed_bonus = fixed_bonus
 
-    def get_commission(self):
+    def calc_commission(self):
         return self.fixed_bonus
 
-    def get_string(self):
+    def __str__(self):
         return f' and receives a bonus commission of {self.fixed_bonus}'
 
-class UnfixedBonus(Commission):
+
+class ContractBonus(Commission):
     def __init__(self, no_contracts, per_contract):
         self.no_contracts = no_contracts
         self.per_contract = per_contract
 
-    def get_commission(self):
+    def calc_commission(self):
         return self.no_contracts * self.per_contract
 
-    def get_string(self):
+    def __str__(self):
         return f' and receives a commission for {self.no_contracts} contract(s) at {self.per_contract}/contract'
+
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = SalaryContract('Billie', 4000)
@@ -70,14 +78,13 @@ billie = SalaryContract('Billie', 4000)
 charlie = HourlyContract('Charlie', 100, 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = SalaryContract('Renee', 3000, UnfixedBonus(4, 200))
+renee = SalaryContract('Renee', 3000, ContractBonus(4, 200))
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = HourlyContract('Jan', 150, 25, UnfixedBonus(3, 220))
+jan = HourlyContract('Jan', 150, 25, ContractBonus(3, 220))
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = SalaryContract('Robbie', 2000, FixedBonus(1500))
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = HourlyContract('Ariel', 120, 30, FixedBonus(600))
-
